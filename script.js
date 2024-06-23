0ea4062e33bb215ea2d864f644b4fd3b27d30384
Ishan <ishanjaiiswal.com>
2024-06-23 19:33:07 +0700

    all files addea

---
+++ script.js
@@ -0,0 +1,15 @@
+let names = document.querySelector("#name");
+let date = document.querySelector("#date");
+let submitButton = document.querySelector("#submit");
+let represent = document.querySelector(".representation");
+let handleClick = () =>{
+let div = document.createElement('div');
+let span = document.createElement('span');
+let span1 = document.createElement('span');
+span.innerHTML=names.value;
+span1.innerHTML=date.value;
+div.append(span,span1);
+represent.appendChild(div);
+
+}
+submitButton.addEventListener('click', handleClick)
