043af647ce829d692753b4985ee5a18dc80a8e60
Ishan <ishanjaiiswal.com>
2024-08-05 04:23:59 -1200

    Changed entire appearence of the Page

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 let names = document.querySelector("#name");
 let date = document.querySelector("#date");
 let submitButton = document.querySelector("#submit");
-let represent = document.querySelector(".representation");
+let resetButton = document.querySelector("#reset");
+let represent = document.querySelector(".result");
+
 let handleClick = () => {
-  let div = document.createElement("div");
-  let span = document.createElement("span");
-  let span1 = document.createElement("span");
+  var div = document.createElement("div");
+  div.setAttribute("class", "representation");
+  var span = document.createElement("span");
+  var span1 = document.createElement("span");
+
   span.innerHTML = names.value;
   span1.innerHTML = date.value;
-  div.append("Task Name : ", span, ",", "Date : ", span1);
+  span.style.color = "green";
+  span1.style.color = "red";
+  div.append(span, span1);
   represent.appendChild(div);
 };
+const resetOperation = () => {
+  represent.innerHTML = "";
+  names.value = "";
+  date.value = "";
+};
 submitButton.addEventListener("click", handleClick);
+resetButton.addEventListener("click", resetOperation);
